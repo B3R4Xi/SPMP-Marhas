699a54e19c7202e950ba19992d24b8de0f2c0b1c
b3r4xi <zeninadif03@gmail.com>
2021-05-24 13:38:21 +0700

    First Commit

---
+++ resources/views/_component/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+    <div class="container-fluid">
+      <div class="copyright float-right">
+        &copy;
+        <script>
+          document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="https://github.com/B3R4Xi" target="_blank">Skripsi Informatika </a>.
+      </div>
+    </div>
+  </footer>
