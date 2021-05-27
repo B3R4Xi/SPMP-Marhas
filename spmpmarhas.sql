967dda6e118b1ce574b5f1f46ecd7e0bc916526b
b3r4xi <zeninadif03@gmail.com>
2021-05-27 18:29:54 +0700

    tambah db export

---
+++ spmpmarhas.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2021 at 01:28 PM
+-- Server version: 10.4.19-MariaDB
+-- PHP Version: 8.0.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spmpmarhas`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_mapel`
+--
+
+CREATE TABLE `tbl_mapel` (
+  `id_mapel` int(11) NOT NULL,
+  `kode_mapel` varchar(124) NOT NULL,
+  `nama_mapel` varchar(124) NOT NULL,
+  `tingkat_mapel` varchar(124) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_mapel`
+--
+
+INSERT INTO `tbl_mapel` (`id_mapel`, `kode_mapel`, `nama_mapel`, `tingkat_mapel`) VALUES
+(1, 'MP01', 'Web Programming 1', '10-RPL'),
+(2, 'MP02', 'Simulasi Digital', '10-RPL');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_mapel`
+--
+ALTER TABLE `tbl_mapel`
+  ADD PRIMARY KEY (`id_mapel`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_mapel`
+--
+ALTER TABLE `tbl_mapel`
+  MODIFY `id_mapel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
