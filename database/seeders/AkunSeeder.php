47b518ccd8f3846297d7878b9ac2ed49ce7e0860
AndrianRamdani <andrianr2802@gmail.com>
2022-02-06 03:05:48 +0700

    edit seeder

--- database/seeders/AkunSeeder.php
+++ database/seeders/AkunSeeder.php
@@ -20,6 +20,8 @@ class AkunSeeder extends Seeder
             [
                 'name'      => 'admin',
                 'email'     => 'admin@mail.com',
+                'alamat'    => 'marhas',
+                'no_hp'     => '0899',
                 'password'   => bcrypt('12345678'),
                 'level_id'     => '1',
 
